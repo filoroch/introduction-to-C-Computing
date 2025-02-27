35192abf5fc3f2af5e4bef4e522d2a9559fcd8da
Filipe <162770790+filoroch@users.noreply.github.com>
2025-02-27 17:46:59 +0000

    Atual

--- entrada-e-saida/saida.c
+++ entrada-e-saida/saida.c
@@ -1,27 +1,9 @@
 #include <stdio.h>
-
+ 
 int main() {
-    char nome[21];        
-    char sobrenome[21];   
     int idade;
-    char areaAtuacao[21];
-
-    printf("Digite seu primeiro nome: \n");
-    scanf("%20s", nome);  
-
-    printf("Digite seu sobrenome: \n");
-    scanf("%20s", sobrenome); 
-
     printf("Digite sua idade: \n");
-    scanf("%d", &idade);  // Adicionado & antes de idade
-
-    printf("Digite sua area de atuação: \n");
-    scanf("%20s", areaAtuacao);
-
-    printf("\n--------------------- FICHA DE CADASTRO ---------------------\n\n");
-    printf("   Nome: %s %s\n", nome, sobrenome);
-    printf("   Idade: %d\n", idade);
-    printf("   Area de Atuação %s\n", areaAtuacao);
-
+    scanf("%d", &idade);
+    printf("Sua idade é: %d\n", idade);
     return 0;
 }
