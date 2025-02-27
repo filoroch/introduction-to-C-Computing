f1bbd209ebe21020507d81bbca9aa99953276a87
Filipe <162770790+filoroch@users.noreply.github.com>
2025-02-27 17:44:46 +0000

    Adicionando: Entrada e Saida
    Signed-off-by: Filipe <162770790+filoroch@users.noreply.github.com>

--- entrada-e-saida/saida.c
+++ entrada-e-saida/saida.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
- 
+
 int main() {
+    char nome[21];        
+    char sobrenome[21];   
     int idade;
+    char areaAtuacao[21];
+
+    printf("Digite seu primeiro nome: \n");
+    scanf("%20s", nome);  
+
+    printf("Digite seu sobrenome: \n");
+    scanf("%20s", sobrenome); 
+
     printf("Digite sua idade: \n");
-    scanf("%d", &idade);
-    printf("Sua idade é: %d\n", idade);
+    scanf("%d", &idade);  // Adicionado & antes de idade
+
+    printf("Digite sua area de atuação: \n");
+    scanf("%20s", areaAtuacao);
+
+    printf("\n--------------------- FICHA DE CADASTRO ---------------------\n\n");
+    printf("   Nome: %s %s\n", nome, sobrenome);
+    printf("   Idade: %d\n", idade);
+    printf("   Area de Atuação %s\n", areaAtuacao);
+
     return 0;
 }
