55555ee1a6618a57f1a5d1f9d9e7d7d26858cb69
filoroch <dev.filoroch@gmail.com>
2025-02-27 06:55:03 -0300

    caos

--- entrada-e-saida/saida.c
+++ entrada-e-saida/saida.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
  
-int main() {
-    int idade;
+/*int cadastro(nome, idade, data_nascimento, peso, altura){
+    printf("Digite o seu nome: \n");
+    scanf("%49s", &nome);
+
     printf("Digite sua idade: \n");
     scanf("%d", &idade);
-    printf("Sua idade é: %d\n", idade);
+
+    printf("Digite sua data de Nascimento no padrão DD/MM/AAAA\n");
+    scanf("%14s", &data_nascimento);
+
+    prinf("Digite o seu peso: \n");
+    scanf("%1f", &peso);
+
+    printf("Digite o seu altura\n");
+    scanf("%1f", &altura);
+
+    printf("\nNome: %s\nIdade: %d", &nome, &idade);
     return 0;
 }
+*/
+
+int main() {
+    char nome[50];
+    int idade;
+    char data_nascimento[12];
+    float peso;
+    double altura;
+    char ocupacao[50];
+    char escolaridade[50];
+
+    printf("----- FICHA DE CADASTRO ----");
+
+    printf("Digite o seu nome: \n");
+    scanf("%c", &nome);
+
+    //printf("Digite sua idade: \n");
+    //scanf("%d", &idade);
+
+    //printf("Digite sua data de Nascimento no padrão DD/MM/AAAA\n");
+    //scanf("%14s", &data_nascimento);
+
+    //prinf("Digite o seu peso: \n");
+    //scanf("%1f", &peso);
+
+    //printf("Digite o seu altura\n");
+    //scanf("%1f", &altura);
+
+    printf("\nNome: %s\n", &nome);
+    return 0;
+};
+
