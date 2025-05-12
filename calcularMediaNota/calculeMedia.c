6c646a94d214b63df6a8651e8f6588d23131da0a
Filipe <162770790+filoroch@users.noreply.github.com>
2025-05-12 21:00:31 +0000

    feat:pushpin: : add calculeMedia.c exercise for repo
    Signed-off-by: Filipe <162770790+filoroch@users.noreply.github.com>

---
+++ calcularMediaNota/calculeMedia.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+O programa precisa calcular a media de tres notas inseridas pelos alunos, então:
+
+- receber notas
+- armazenar os valores
+- calcular as notas recursivamente
+
+*/
+
+int recebeNotas(){
+    float nota = 0;
+
+    printf("Digite sua nota")
+    scanf("%f", nota)
+
+    calculeMedia()
+}
+
+int calculeMedia(float x1, float x2, float x3){
+    float mediaAritimetica = ( x1 + x2 + x3 ) / 3
+    return mediaAritimetica
+}
+
+int main(){
+    int mediaFinal = 0;
+    int i = 0
+    while (mediaFinal == 0 && 3)
+    {
+        recebeNotas()
+        i += 1
+    }
+    
+}
